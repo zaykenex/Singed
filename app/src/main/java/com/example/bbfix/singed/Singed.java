139c954ff1e930b5ef8693dda2b6552627fc4260
zaykenex <zaykenex@gmail.com>
2017-07-08 13:49:51 -0500

    Initial commit

---
+++ app/src/main/java/com/example/bbfix/singed/Singed.java
@@ -0,0 +1,13 @@
+package com.example.bbfix.singed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Singed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singed);
+    }
+}
